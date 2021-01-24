01774889d1141e61b69d906c43d67d85e55bd858
BuildTools <unconfigured@null.spigotmc.org>
2021-01-24 11:47:29 +0200

    champion data

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ let idInfo = {
 	pName:   null,
 	region:  null,
 	accId:   null,
+	sumId:   null,
 };
 let gameId = []; // this is the empty array storing all game ids of the last 10 games(0-9)
 let gameData = [];
-
+let champData = [];
 //callbacks
 function setPlayerInfo(data){ //this sets the accId
 	idInfo.accId = data.accountId;
+	idInfo.sumId = data.id;
 }
 function setMatchId(data){ //this sets all the game ids
 	gameId.length = 0; // this clears the array every time when you submit, prefents game stacking
 	for(let i = 0; i <= 9; i++){
 		gameId.push(data.matches[i].gameId);
+	}
+}
+function setChampionData(data){
+	champData.length = 0;
+	for(let i = 0; i <= 2; i++){
+		champData.push(data[i]);
 	}
 }
 //callbacks
@@ -29,17 +37,27 @@ async function getMatchData(id){
 	const response = await fetch(`http://31.13.206.166:42069/lol/match/v4/matches/${id}?platform=${idInfo.region}`)
 	return response.json();
 }
+async function getPlayerData(){
+	await fetch(`http://31.13.206.166:42069/lol/summoner/v4/summoners/by-name/${idInfo.pName}?platform=${idInfo.region}`)
+	.then(res => res.json())
+	.then(data => setPlayerInfo(data))
+}
+async function getChampionData(){
+	await fetch(`http://31.13.206.166:42069/lol/champion-mastery/v4/champion-masteries/by-summoner/${idInfo.sumId}?platform=${idInfo.region}`)
+	.then(res => res.json())
+	.then(data => setChampionData(data))
+}
 //get functions
 async function submitInfo(){ //this gets the player accId
 
 	idInfo.pName = document.getElementById("nameInput").value;
 	idInfo.region = document.getElementById("region").value;
 	
-	await fetch(`http://31.13.206.166:42069/lol/summoner/v4/summoners/by-name/${idInfo.pName}?platform=${idInfo.region}`)
-	.then(res => res.json())
-	.then(data => setPlayerInfo(data))
+	await getPlayerData()
 
 	await getMatchId()
+
+	getChampionData();
 
 	gameData.length = 0;
 	for(let i = 0; i <= 9; i++){
